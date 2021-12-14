395b06d2cb534f7d8893f86ca8bc5b8f47ddf778
6tranov <6tranov@yahoo.co.jp>
2021-12-15 03:55:48 +0900

    :+1:feat add siteTemplate.ts
    that is all.
    
      - :+1:feat
        A new feature
      - :bug:fix
        A bug fix
      - :memo:docs
        Documentation only changes
      - :lipstick:style
        Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
      - :recycle:refactor
        A code change that neither fixes a bug nor adds a feature
      - :racehorse:perf
        A code change that improves performance
      - :white_check_mark:test
        Adding missing or correcting existing tests
      - :construction_worker:chore
        Changes to the build process or auxiliary tools and libraries such as documentation generation

---
+++ src/siteTemplate.ts
@@ -0,0 +1,14 @@
+import { BackgroundEvent } from './backgroundEvent';
+import { ContentEvent } from './contentEvent';
+import { PageActionMaker } from './pageActionMaker';
+
+BackgroundEvent.enablePageActionEvent.Trigger(null, null, null, null);
+ContentEvent.enableContentScriptEvent.addHandler(new PageActionMaker(getURLs, null).contentHandler);
+
+//#region Functions
+function getURLs(_message: any, _sender: any, _sendRespons: any) {
+    let URLs: string[] = new Array<string>();
+
+    return URLs;
+}
+//#endregion
