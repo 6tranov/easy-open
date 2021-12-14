04a6b995b3ea360a4f26fb99041330108c1efbf0
6tranov <6tranov@yahoo.co.jp>
2021-12-15 03:23:55 +0900

    :+1:feat add export in bgEvent
    use backgroundEvent in other ts files
    
      - :+1:feat
        A new feature
      - :bug:fix
        A bug fix
      - :memo:docs
        Documentation only changes
      - :lipstick:style
        Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
      - :recycle:refactor
        A code change that neither fixes a bug nor adds a feature
      - :racehorse:perf
        A code change that improves performance
      - :white_check_mark:test
        Adding missing or correcting existing tests
      - :construction_worker:chore
        Changes to the build process or auxiliary tools and libraries such as documentation generation

--- src/backgroundEvent.ts
+++ src/backgroundEvent.ts
@@ -59,3 +59,5 @@ class BackgroundEvent {
     }
     //#endregion
 }
+
+export { BackgroundEvent };
---
+++ src/pageActionMaker.ts
@@ -0,0 +1,60 @@
+import { Validation } from './validation';
+import { BackgroundEvent } from './backgroundEvent';
+
+class PageActionMaker {
+    //#region Field
+    private getURLs;
+    //#endregion
+
+    //#region Constructor
+    constructor(getURLs, isCorrectPage) {
+        //#region Input Validation
+        if (Validation.isNullOrUndefined(getURLs)) throw new Error("getURLs is null or undefined.");
+        this.getURLs = getURLs;
+        if (!Validation.isNullOrUndefined(isCorrectPage)) {
+            this.isCorrectPage = isCorrectPage;
+        }
+        //#endregion
+    }
+    //#endregion
+
+    static getNewURLs(excludedURLs, URLs) {
+        //#region Inuput Validation
+        Validation.NullUndefinedCheck(excludedURLs, "excludedURLs is null or undefined.")
+        Validation.NullUndefinedCheck(URLs, "URLs is null or undefined.")
+        //#endregion
+        //引数のURLsから、既に開いているURLsを取り除く。
+        return URLs.filter(url => excludedURLs.indexOf(url) == -1);
+    }
+    isCorrectPage(_message: any, _sender: any, _sendResponse: any) {
+        return true;
+    }
+    get contentHandler() {
+        return (message, sender, sendResponse) => {
+            if (!this.isCorrectPage(message, sender, sendResponse)) {
+                BackgroundEvent.notifyNoContentToOpenEvent.Trigger(null, null, null, null);
+                return;
+            }
+            //#region Input Validation
+            if (Validation.isNullOrUndefined(message.excludedURLs)) throw new Error("message.excludedURLs is null or undefined.");
+            let excludedURLs = message.excludedURLs;
+            if (Validation.isNullOrUndefined(message.tabLength)) throw new Error("message.tabLength is null or undefined.");
+            let tabLength = message.tabLength;
+            let URLs = this.getURLs(message, sender, sendResponse);
+            let newURLs = PageActionMaker.getNewURLs(excludedURLs, URLs);
+            if (Validation.isNullOrUndefined(newURLs.length)) throw new Error("newURLs.length is null or undefined.");
+            //#endregion
+            if (newURLs.length > 0) {
+                if (tabLength === 1) {
+                    BackgroundEvent.openURLsWhenSingleTabEvent.Trigger(null, { newURLs: newURLs }, null, null);
+                } else if (tabLength > 1) {
+                    BackgroundEvent.openURLsWhenMultipleTabsEvent.Trigger(null, { newURLs: newURLs }, null, null);
+                }
+            } else {
+                BackgroundEvent.notifyNoContentToOpenEvent.Trigger(null, null, null, null);
+            }
+        }
+    }
+}
+
+export { PageActionMaker };
