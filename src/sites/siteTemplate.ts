581c9effe246f540f92fcb7f826d78ad9a766816
6tranov <6tranov@yahoo.co.jp>
2021-12-15 20:37:29 +0900

    :bug:fix fix siteTemplate.ts
    fix paths of import(./*** to ../***)
    
      - :+1:feat
        A new feature
      - :bug:fix
        A bug fix
      - :memo:docs
        Documentation only changes
      - :lipstick:style
        Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
      - :recycle:refactor
        A code change that neither fixes a bug nor adds a feature
      - :racehorse:perf
        A code change that improves performance
      - :white_check_mark:test
        Adding missing or correcting existing tests
      - :construction_worker:chore
        Changes to the build process or auxiliary tools and libraries such as documentation generation

--- src/sites/siteTemplate.ts
+++ src/sites/siteTemplate.ts
@@ -1,6 +1,6 @@
 import { BackgroundEvent } from '../backgroundEvent';
 import { ContentEvent } from '../contentEvent';
-import { PageActionMaker } from '..package-lock.json/pageActionMaker';
+import { PageActionMaker } from '../pageActionMaker';
 
 BackgroundEvent.enablePageActionEvent.Trigger(null, null, null, null);
 ContentEvent.enableContentScriptEvent.addHandler(new PageActionMaker(getURLs, null).contentHandler);
