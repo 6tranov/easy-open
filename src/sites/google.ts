29b6cff8f077694a4f2cbcec116867e4e5d6fdac
6tranov <6tranov@yahoo.co.jp>
2021-12-15 20:39:48 +0900

    :bug:fix add return value of getURLs in siteTmplt
    URLs is string[]
    
      - :+1:feat
        A new feature
      - :bug:fix
        A bug fix
      - :memo:docs
        Documentation only changes
      - :lipstick:style
        Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
      - :recycle:refactor
        A code change that neither fixes a bug nor adds a feature
      - :racehorse:perf
        A code change that improves performance
      - :white_check_mark:test
        Adding missing or correcting existing tests
      - :construction_worker:chore
        Changes to the build process or auxiliary tools and libraries such as documentation generation

--- src/sites/google.ts
+++ src/sites/google.ts
@@ -9,6 +9,18 @@ ContentEvent.enableContentScriptEvent.addHandler(new PageActionMaker(getURLs, nu
 function getURLs(_message: any, _sender: any, _sendRespons: any) {
     let URLs: string[] = new Array<string>();
 
+    let elements = document.getElementsByTagName("a");
+    for (let i = 0; i < elements.length; i++) {
+        if (
+            elements[i].getAttribute("href") != null &&
+            elements[i].getAttribute("data-ved") != null &&
+            elements[i].getAttribute("ping") != null
+        ) {
+            let url = elements[i].getAttribute("href");
+            URLs.push(url);
+        }
+    }
+
     return URLs;
 }
 //#endregion
--- src/sites/siteTemplate.ts
+++ src/sites/siteTemplate.ts
@@ -6,7 +6,7 @@ BackgroundEvent.enablePageActionEvent.Trigger(null, null, null, null);
 ContentEvent.enableContentScriptEvent.addHandler(new PageActionMaker(getURLs, null).contentHandler);
 
 //#region Functions
-function getURLs(_message: any, _sender: any, _sendRespons: any) {
+function getURLs(_message: any, _sender: any, _sendRespons: any): string[] {
     let URLs: string[] = new Array<string>();
 
     return URLs;
