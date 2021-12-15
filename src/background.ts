6357bb941eeb6dfaf62bc92444033c7b0c1fc9d0
6tranov <6tranov@yahoo.co.jp>
2021-12-15 20:32:57 +0900

    :+1:feat add background.ts
    that is all
    
      - :+1:feat
        A new feature
      - :bug:fix
        A bug fix
      - :memo:docs
        Documentation only changes
      - :lipstick:style
        Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
      - :recycle:refactor
        A code change that neither fixes a bug nor adds a feature
      - :racehorse:perf
        A code change that improves performance
      - :white_check_mark:test
        Adding missing or correcting existing tests
      - :construction_worker:chore
        Changes to the build process or auxiliary tools and libraries such as documentation generation

--- src/background.ts
+++ src/background.ts
@@ -6,21 +6,21 @@ import { ContentEvent } from './contentEvent';
 
 let eum = new ExcludedURLManager();
 
-BackgroundEvent.enablePageActionEvent.addHandler(func1);
-BackgroundEvent.openURLsWhenSingleTabEvent.addHandler(func2);
-BackgroundEvent.openURLsWhenMultipleTabsEvent.addHandler(func3);
-BackgroundEvent.notifyNoContentToOpenEvent.addHandler(func4);
+BackgroundEvent.enablePageActionEvent.addHandler(enablePageActionHandler);
+BackgroundEvent.openURLsWhenSingleTabEvent.addHandler(openURLsWhenSingleTabHandler);
+BackgroundEvent.openURLsWhenMultipleTabsEvent.addHandler(openURLsWhenMultipleTabsHandler);
+BackgroundEvent.notifyNoContentToOpenEvent.addHandler(notifyNoContentToOpenHandler);
 
 chrome.pageAction.onClicked.addListener(clickIcon);
 
 
 //#region Functions
-function func1(_message: any, sender: any, _sendResponse: any) {
+function enablePageActionHandler(_message: any, sender: any, _sendResponse: any) {
     //#region Input Validation
     Validation.NullUndefinedCheck(sender, "sender is null or undefined");
     Validation.NullUndefinedCheck(sender.tab, "sender.tab is null or undefined.");
     Validation.NullUndefinedCheck(sender.tab.id, "sender.tab.id is null or undefined.");
-    Validation.NumberCheck(sender.tab.id, "typeof sender.tab.id is not number");
+    Validation.numberCheck(sender.tab.id, "typeof sender.tab.id is not number.");
     //#endregion
     let tabId: number = sender.tab.id;
     enablePageAction(tabId);
@@ -28,14 +28,17 @@ function func1(_message: any, sender: any, _sendResponse: any) {
     //#region Local Functions
     function enablePageAction(tabId: number) {
         //#region Input Validation
-        if (Validation.isNullOrUndefined(tabId)) throw new Error("tabId is null or undefined.");
+        Validation.NullUndefinedCheck(tabId, "tabId is null or undefined.");
+        Validation.numberCheck(tabId, "tabId is not number.");
         //#endregion
         disablePopup(tabId);
         chrome.pageAction.show(tabId, null);
+
         //#region Local Functions
-        function disablePopup(tabId) {
+        function disablePopup(tabId: number) {
             //#region Input Validation
-            if (Validation.isNullOrUndefined(tabId)) throw new Error("tabId is null or undefined.");
+            Validation.NullUndefinedCheck(tabId, "tabId is null or undefined.");
+            Validation.numberCheck(tabId, "tabId is not number.");
             //#endregion
             chrome.pageAction.setPopup({ popup: "", tabId: tabId });
         }
@@ -43,42 +46,61 @@ function func1(_message: any, sender: any, _sendResponse: any) {
     }
     //#endregion
 }
-function func2(message, sender, _sendResponse) {
+function openURLsWhenSingleTabHandler(message: any, sender: any, _sendResponse: any) {
     //#region Input Validation
-    if (Validation.isNullOrUndefined(sender)) throw new Error("sender is null or undefined.");
-    if (Validation.isNullOrUndefined(sender.tab)) throw new Error("sender.tab is null or undefined.");
-    let tab = sender.tab;
-    if (Validation.isNullOrUndefined(message.newURLs)) throw new Error("message.newURLs is null or undefined.");
+    Validation.NullUndefinedCheck(sender, "sender is null or undefined.");
+    Validation.NullUndefinedCheck(sender.tab, "sender.tab is null or undefined.");
+    Validation.NullUndefinedCheck(message.newURLs, "message.newURLs is null or undefined.");
+    //#endregion
+    //#region Initialization
+    let tab: any = sender.tab;
     let newURLs = message.newURLs;
     //#endregion
     openURLs(tab, newURLs, true);
 }
-function func3(message, sender, _sendResponse) {
+function openURLsWhenMultipleTabsHandler(message: any, sender: any, _sendResponse: any) {
     //#region Input Validation
-    if (Validation.isNullOrUndefined(sender)) throw new Error("sender is null or undefined.");
-    if (Validation.isNullOrUndefined(sender.tab)) throw new Error("sender.tab is null or undefined.");
-    let tab = sender.tab;
-    if (Validation.isNullOrUndefined(message.newURLs)) throw new Error("message.newURLs is null or undefined.");
+    Validation.NullUndefinedCheck(sender, "sender is null or undefined.");
+    Validation.NullUndefinedCheck(sender.tab, "sender.tab is null or undefined.");
+    Validation.NullUndefinedCheck(message.newURLs, "message.newURLs is null or undefined.");
+    //#endregion
+    //#region Initialization
+    let tab: any = sender.tab;
     let newURLs = message.newURLs;
     //#endregion
     openURLs(tab, newURLs, false);
 }
-function func4(_message, _sender, _sendResponse) {
+function notifyNoContentToOpenHandler(_message: any, _sender: any, _sendResponse: any) {
     notifyNoContentToOpen();
-    //#region Local Functions
-    function notifyNoContentToOpen() {
-        alert('開くコンテンツがありません。');
-    }
-    //#endregion
 }
-function openURLs(tab, URLs, isSingleTab) {
+function clickIcon(tab: any) {
     //#region Input Validation
-    if (Validation.isNullOrUndefined(tab)) throw new Error("tab is null or undefined.");;
-    if (Validation.isNullOrUndefined(tab.id)) throw new Error("tab.id is null or undefined.");
+    Validation.NullUndefinedCheck(tab, "tab is null or undefined.");
+    Validation.NullUndefinedCheck(tab.id, "tab.id is null or undefined.");
+    Validation.NullUndefinedCheck(tab.windowId, "tab.windowId is null or undefined.");
+    //#endregion
+    //#region Initialization
     let tabId = tab.id;
-    if (Validation.isNullOrUndefined(URLs)) throw new Error("URLs is null or undefined.");
-    if (Validation.isNullOrUndefined(URLs.length)) throw new Error("URLs.length is null or undefined.");
-    if (Validation.isNullOrUndefined(isSingleTab)) throw new Error("isSingleTab is null or undefined.");
+    let windowId = tab.windowId;
+    let excludedURLs = eum.excludedURLs(tabId);
+    //#endregion
+    chrome.tabs.query({ windowId: windowId }, (tabs) => {
+        ContentEvent.enableContentScriptEvent.Trigger(tabId, { excludedURLs: excludedURLs, tabLength: tabs.length }, null, null);
+    });
+}
+function notifyNoContentToOpen() {
+    alert('開くコンテンツがありません。');
+}
+function openURLs(tab: any, URLs, isSingleTab: boolean) {
+    //#region Input Validation
+    Validation.NullUndefinedCheck(tab, "tab is null or undefined.");
+    Validation.NullUndefinedCheck(tab.id, "tab.id is null or undefined.");
+    Validation.NullUndefinedCheck(URLs, "URLs is null or undefined.");
+    Validation.NullUndefinedCheck(URLs.length, "URLs.length is null or undefined.");
+    Validation.NullUndefinedCheck(isSingleTab, "isSingleTab is null or undefined.");
+    //#endregion
+    //#region Initialization
+    let tabId = tab.id;
     //#endregion
     if (URLs.length == 0) {
         notifyNoContentToOpen();
@@ -104,21 +126,21 @@ function openURLs(tab, URLs, isSingleTab) {
         });
     }
     //#region Local Functions
-    function hilight(windowId, newOpenedURLsLength) {
+    function hilight(windowId: number, newOpenedURLsLength: number) {
         //#region Input Validation
-        if (Validation.isNullOrUndefined(windowId)) throw new Error("windowId is null or undefined.");
+        Validation.NullUndefinedCheck(windowId, "windowId is null or undefined.");
         //#endregion
         chrome.tabs.query({ windowId: windowId }, (tabs) => {
             let index = tabs.length - newOpenedURLsLength;
             chrome.tabs.highlight({ tabs: index });
         });
     }
-    function open(url, fromTab, callback) {
+    function open(url: string, fromTab: any, callback) {
         //#region Input Validation
-        if (Validation.isNullOrUndefined(url)) throw new Error("url is null or undefined.");
-        if (Validation.isNullOrUndefined(fromTab)) throw new Error("fromTab is null or undefined.");
-        if (Validation.isNullOrUndefined(fromTab.windowId)) throw new Error("fromTab.windowId is null or undefined.");
-        if (Validation.isNullOrUndefined(callback)) throw new Error("callback is null or undefined.");
+        Validation.NullUndefinedCheck(url, "url is null or undefined.");
+        Validation.NullUndefinedCheck(fromTab, "fromTab is null or undefined.");
+        Validation.NullUndefinedCheck(fromTab.windowId, "fromTab.windowId is null or undefined.");
+        Validation.NullUndefinedCheck(callback, "callback is null or undefined.");
         //#endregion
         chrome.tabs.create({ url: url }, tab => {
             if (Validation.isNullOrUndefined(tab)) alert('tab is null or undefined');
@@ -129,18 +151,5 @@ function openURLs(tab, URLs, isSingleTab) {
         });
     }
     //#endregion
-}
-function clickIcon(tab) {
-    //#region Input Validation
-    if (Validation.isNullOrUndefined(tab)) throw new Error("tab is null or undefined.");
-    if (Validation.isNullOrUndefined(tab.id)) throw new Error("tab.id is null or undefined.");
-    if (Validation.isNullOrUndefined(tab.windowId)) throw new Error("tab.windowId is null or undefined.");
-    let tabId = tab.id;
-    let windowId = tab.windowId;
-    let excludedURLs = eum.excludedURLs(tabId);
-    //#endregion
-    chrome.tabs.query({ windowId: windowId }, (tabs) => {
-        ContentEvent.enableContentScriptEvent.Trigger(tabId, { excludedURLs: excludedURLs, tabLength: tabs.length }, null, null);
-    });
 }
 //#endregion
